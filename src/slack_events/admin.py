738552f2a80cb6a29e09eb03fb405da770ce63bf
Stefan Dworschak <stef.dworschak@gmail.com>
2020-10-04 17:56:31 +0100

    Adding view logic into advanced task (#3)

--- src/slack_events/admin.py
+++ src/slack_events/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import RawSlackEvent, SlackEvent
+from .models import RawSlackEvent, SlackEvent, XApiStatement
+
+
+class XApiStatementAdmin(admin.ModelAdmin):
+    model = XApiStatement
+    list_display = ('slack_event', 'delivered',)
+    list_filter = ('slack_event', 'delivered',)
 
 
 admin.site.register(RawSlackEvent)
 admin.site.register(SlackEvent)
+admin.site.register(XApiStatement, XApiStatementAdmin)
--- src/slack_events/tasks.py
+++ src/slack_events/tasks.py
@@ -4,27 +4,43 @@ import logging
 import requests
 
 from celery import shared_task
-from slack_events.models import XApiStatement
+from slack_events.models import SlackEvent, XApiStatement
 from xapi.models import LrsConfig
 
 log = logging.getLogger(__name__)
 
 
 @shared_task
-def send_xapi_statement_to_lrs(xapi_statement, slack_event):
-    log.info(xapi_statement)
+def schedule_xapi_task(payload):
+    """ Handle Slack Events Subscription Payload to xAPI conversion and 
+    delivery to one (or multiple) LRS """
+    slack_event = SlackEvent(_payload=json.dumps(payload))
+    slack_event.save()
+
+    xapi_statement = slack_event.slack_event_to_xapi_statement()
+
+    if not xapi_statement:
+        log.exception("xAPI Statement could not be generated")
+        return
+
     lrs_configs = LrsConfig.objects.all()
     if not lrs_configs:
         log.exception("No LRS defined yet. Cannot send xAPI statment")
         return
 
+    for lrs_config in lrs_configs:
+        send_xapi_statement_to_lrs(lrs_config, xapi_statement, slack_event)
+    return
+
+
+def send_xapi_statement_to_lrs(lrs_config, xapi_statement, slack_event):
+    """ Sends an xAPI statement to an LRS """
     headers = {'Content-type': 'application/json;charset=UTF-8',
                'x-experience-api-version': '1.0.1'}
-
-    res = requests.post(lrs_configs[0].lrs_endpoint,
+    res = requests.post(lrs_config.lrs_endpoint,
                         data=json.dumps(xapi_statement, default=str),
-                        auth=(lrs_configs[0].lrs_auth_user,
-                              lrs_configs[0].lrs_auth_pw),
+                        auth=(lrs_config.lrs_auth_user,
+                              lrs_config.lrs_auth_pw),
                         headers=headers)
     if res.status_code != 200:
         log.exception(res.content)
