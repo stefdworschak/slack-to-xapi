7c55e6f1252dbf1bccffa8315c80fc2f2d2dec5e
Stefan Dworschak <stef.dworschak@gmail.com>
2020-10-10 19:59:03 +0100

    Adding nginx, seeding and readme changes

--- scripts/run_seed.sh
+++ scripts/run_seed.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd "$(dirname "$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)" )"  # cd into parent dir
-python3 manage.py loaddata $1
+docker-compose exec webapp python3 manage.py loaddata $1
