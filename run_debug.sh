03344018144a4ae72d39bfbb41459517195b738b
Stefan Dworschak <stef.dworschak@gmail.com>
2020-10-10 16:09:23 +0100

    Moving scripts

--- run_debug.sh
+++
@@ -1,6 +0,0 @@
-docker run --rm -it \
--p "8000:8000" \
---mount "type=bind,src=/home/stefan/Documents/slack-to-xapi/src/slack_events/,dst=/app/slack_events/" \
---mount "type=bind,src=/home/stefan/Documents/slack-to-xapi/src/xapi/,dst=/app/xapi/" \
---mount "type=bind,src=/home/stefan/Documents/slack-to-xapi/data/,dst=/app/data/" \
---name slack-to-xapi slack-to-xapi
--- run_tests.sh
+++
@@ -1,1 +0,0 @@
-docker-compose exec webapp ./manage.py test
